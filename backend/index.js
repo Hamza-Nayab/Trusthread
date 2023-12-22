5285f613a1e50ad17037952b07e476226eae4630
Hamza-Nayab <Hamza.nayab48@gmail.com>
2023-12-21 23:44:47 -0800

    asdfas

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,41 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js";
-import reviewRoutes from "./routes/reviewRoutes.js";
-import cors from "cors";
+
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import reviewRoutes from './routes/reviewRoutes.js';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
-app.use(
-  cors({
-    origin: ["https://trusthread-eiao.vercel.app"],
-    // origin: ["http://localhost:3000"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // Connect to MongoDB
 const url =
   "mongodb+srv://hamaz:hamza123@cluster0.sgog47j.mongodb.net/Thread?retryWrites=true&w=majority";
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
-  });
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
 
 // Middleware
 app.use(express.json());
 // Add other middleware as needed
 
-app.use("/api/users", userRoutes);
-app.use("/api", reviewRoutes);
-app.use("/api/images", reviewRoutes);
-app.get("/", (req, res) => {
+app.use('/api/users', userRoutes);
+app.use('/api', reviewRoutes);
+app.use('/api/images', reviewRoutes);
+app.get("/",(req,res)=>{
   res.send("Running!");
-});
+})
+
 
 // Routes
 // Import and use your defined routes here
